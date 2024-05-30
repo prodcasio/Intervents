3359f0038eb75919c43fa8e774e1c522671e10fa
5ciin-02 <prodcasio@gmail.com>
2024-05-30 11:04:13 +0200

    c

--- app/src/main/java/com/app/intervents/MainActivity.java
+++ app/src/main/java/com/app/intervents/MainActivity.java
@@ -2,12 +2,18 @@ package com.app.intervents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText nome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
