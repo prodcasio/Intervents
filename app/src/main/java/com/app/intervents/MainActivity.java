ea1e66a0992c10f02d7472b2d65e5b2bfeffe556
adolf <fra.rodella1@gmail.com>
2024-05-30 11:24:11 +0200

    mainactivity

--- app/src/main/java/com/app/intervents/MainActivity.java
+++ app/src/main/java/com/app/intervents/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.intervents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
@@ -7,13 +8,19 @@ import android.view.*;
 
 
 public class MainActivity extends AppCompatActivity {
-    EditText nome;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void insertIntervent(View v){
+        Intent intent = new Intent(this, insertIntervent.class);
+        startActivity(intent);
+    }
 
+    public void viewIntervents(View v){
+        Intent intent = new Intent(this, VisualizzaInterventi.class);
+        startActivity(intent);
     }
 }
