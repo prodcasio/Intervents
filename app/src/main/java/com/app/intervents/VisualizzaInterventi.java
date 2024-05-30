47440e61ffae9e65e45a40f6aa0a1e15d4f01cfb
5ciin-02 <prodcasio@gmail.com>
2024-05-30 12:48:14 +0200

    c

--- app/src/main/java/com/app/intervents/VisualizzaInterventi.java
+++ app/src/main/java/com/app/intervents/VisualizzaInterventi.java
@@ -3,6 +3,8 @@ package com.app.intervents;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -16,6 +18,7 @@ import java.util.Scanner;
 public class VisualizzaInterventi extends AppCompatActivity {
 
     TextView test;
+    LinearLayout ll = (LinearLayout) findViewById(R.id.lista);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,16 @@ public class VisualizzaInterventi extends AppCompatActivity {
 
         File cartella = getFilesDir();
         try {
+            /*
             FileReader fr = new FileReader(cartella.getAbsolutePath() + "/interventi.csv");
             BufferedReader br = new BufferedReader(fr);
-            Log.d("Miotag", br.readLine());
+            */
+            File file = new File(cartella.getAbsolutePath() + "/interventi.csv");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNext()){
+                View view = new View(this);
+                ll.addView(view);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
